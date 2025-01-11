ae9632c8ecfed55fc51bd2949fcd105103be62af
stimado9021 <quinteroenrrique321@gmail.com>
2025-01-10 19:07:15 -0500

    feat:likst teachers ona again

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return 'probando que vamos a subir';
+  getTeachers(): string {
+    return this.appService.teachers();
   }
 }
