2f50dfe10e0c0beced5c2837c114f4abf4b26cc2
nestorrubiano <link252009@gmail.com>
2025-01-21 16:56:53 -0500

    ok

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,17 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterModule } from '@angular/router';
-
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-})
-export class FooterComponent {
-  currentYear = new Date().getFullYear();
-=======
 
 @Component({
   selector: 'app-footer',
@@ -21,5 +8,4 @@ export class FooterComponent {
 })
 export class FooterComponent {
 
->>>>>>> aa6fd92a496973690a65fdfef0055b63d1403f28
 }
