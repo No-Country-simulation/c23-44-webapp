1fa736ba49f7c878846dc35acbc80f909152ae07
Paul Picon <jonathan.picon20@gmail.com>
2025-02-06 18:45:32 -0500

    Realse fix footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
@@ -11,15 +11,5 @@ import { RouterModule } from '@angular/router';
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
-=======
-
-@Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
-})
-export class FooterComponent {
-
->>>>>>> 2f50dfe (ok)
 }
+
